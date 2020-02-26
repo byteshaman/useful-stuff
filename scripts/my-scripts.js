6824e7618d4415ed1dae254271011da86b786aaf
n00bCod3r <n00bCod3r@protonmail.com>
2020-02-26 15:46:01 +0100

    Added personal scripts

---
+++ scripts/my-scripts.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+	$('li a').attr({
+		data-toggle: 'popover',
+		target: '_blank'
+	});
+
+	$('[data-toggle="popover"]').popover({
+		placement: 'right',
+		trigger: 'hover'
+	});
+});
