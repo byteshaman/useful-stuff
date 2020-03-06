746861d4350bbc0fc6bc2a569ed254bf56347ffe
n00bCod3r <n00bCod3r@protonmail.com>
2020-03-06 20:23:52 +0100

    Improved CSS and JS scripts

--- scripts/my-scripts.js
+++ scripts/my-scripts.js
@@ -1,41 +1,71 @@
-﻿$(document).ready(function () {
+﻿//add data-toggle="popover" to elem	
+function makePoppable(elem) {
+	elem.attr('data-toggle', 'popover');
+	//personalize the pop effect
+	elem.popover({
+		placement: 'right',
+		trigger: 'hover'
+	});
+}
 
-	//add data-toggle="popover" to elem
-	function makePoppable(elem) {
-		elem.attr('data-toggle', 'popover');
-		//personalize the pop effect
-		elem.popover({
-			placement: 'right',
-			trigger: 'hover'
-		});
-	}
+function formatWell(elem) {
+	var address = new URL(elem.attr('href'));
+	var hostname = address.hostname;
+	if (hostname.indexOf('www')>-1) //if hostname contains 'www'
+		hostname = hostname.substring(4); //remove it
+	return hostname;
+}
 
-	function formatWell(elem) {
-		var address = new URL(elem.attr('href'));
-		var hostname = address.hostname;
-		if (hostname.indexOf('www')>-1) //if hostname contains 'www'
-			hostname = hostname.substring(4); //update it removing 'www.'
-		return hostname;
-	}
+function setDataContent(elem) {
+	makePoppable(elem);
+	var websiteName = formatWell(elem);
+	//set data-content to custom values for most used links
+	if (websiteName.indexOf('github.') > -1)
+		websiteName = "github";
+	else if (websiteName.indexOf('css-tricks') > -1)
+		websiteName = "css-tricks.com";
+	else if (websiteName.indexOf('w3schools') > -1)
+		websiteName = "w3schools";
+	else if (websiteName.indexOf('github.') > -1)
+		websiteName = "github";
 
-	function setDataContent(elem) {
-		makePoppable(elem);
-		var websiteName = formatWell(elem);
-		//set data-content to custom values for most used links
-		if (websiteName.indexOf('github.') > -1)
-			websiteName = "github";
-		else if (websiteName.indexOf('css-tricks') > -1)
-			websiteName = "css-tricks.com";
-		else if (websiteName.indexOf('w3schools') > -1)
-			websiteName = "w3schools";
-		else if (websiteName.indexOf('github.') > -1)
-			websiteName = "github";
+	elem.attr('data-content', websiteName);
+}
 
-		elem.attr('data-content', websiteName);
-	}
+//function loadNav() {
+//    $('#menu').load('../pages/menu.html');
+//}
 
-	//make list links open in a new tab
-	$('li a').attr('target', '_blank');
+function moveBodySidebarDown () {
+	$('body').css('margin-top',navH);
+	$('body').attr('data-offset',navH + 5); //to apply the "active" class properly
+
+	//positioning the navbar's links
+	$('#sidebar>nav').css('top',navH); 
+}
+
+//every time the user clicks on a link whose href starts with '#', scroll to it keeping in mind the fixed navbar's height
+function moveAnchorDown() {
+	$('a[href^="#"]').click(function () {
+		let target = $($(this).attr('href'));
+		let offsetTop = target.offset().top;
+		let scrollto = offsetTop - navH;
+		console.log(scrollto);
+	$('html, body').animate({scrollTop:scrollto}, 5);
+	});
+}
+
+//global variable
+var navH;
+
+$(document).ready(function () {
+	navH = $('#navbar').innerHeight(); //assign navH a value after it's loaded
+	moveBodySidebarDown();
+	moveAnchorDown();
+	
+
+	//make list links inside element with id="content" open in a new tab
+	$('#content li a').attr('target', '_blank');
 
 	//scan every list link which hasn't a 'multi' class
 	$('li:not(.multi) a').each(function () {
@@ -46,11 +76,6 @@
 		else {
 			setDataContent($(this));
 		}
-		
-
-		
-		//indexOf returns -1 if the string passed as parameter is not found
-		
 	});
 
 	//scan every list link with multi class
@@ -59,9 +84,5 @@
 		$(this).text(textVal); //assigns the formatted URL value to the link's text
 	});
 
-
-		let navH = $('#navbar').innerHeight();
-		$('body').css('margin-top',navH);
-
-
 });
+
