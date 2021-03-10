e9909682db84f9853c22f1f7ad9f07b1facfcd9a
n00bCod3r <n00bCod3r@protonmail.com>
2021-03-10 14:53:12 +0100

    GIF load fix, maybe

--- scripts/show-preloader.js
+++ scripts/show-preloader.js
@@ -1,5 +1,3 @@
-
-
 const loadDiv = document.createElement('div');
 loadDiv.id = 'loader';
 document.querySelector('body').prepend(loadDiv);
@@ -8,5 +6,5 @@ loadDiv.style.position = 'fixed';
 loadDiv.style.width = '100%';
 loadDiv.style.height = '100vh';
 loadDiv.style.zIndex = '2000'; //bootstrap navbar is 1030
-loadDiv.style.background = `#161519 url('../imgs/loading.gif') no-repeat center`;
+loadDiv.style.background = `#161519 url('./imgs/loading.gif') no-repeat center`;
 
